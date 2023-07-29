0a9dccc0e8573d970bbb539fa4b6f2d5950d6402
AshishkumarSamantara <132549546+AshishkumarSamantara@users.noreply.github.com>
2023-07-29 19:25:44 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://ashishkumarsamantara:chandan@cluster0.vrznndw.mongodb.net/placementcell');
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect('mongodb+srv://soumyasri2245:Soumya22%4034@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Error in database connection!!!'));
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
-db.once('open', () => {
-    console.log('Connection to database is succesfull.');
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
 });
 
 module.exports = db;
